9f905874f44dfd10ae1343a0e0c4de22cb871d4b
Dan23H <daniel.hernandez_val@uao.edu.co>
2025-05-12 05:19:43 -0500

    Quick Fixes

---
+++ frontend/src/app/new/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import GameForm from '../components/GameForm'
+
+export default function NewPage() {
+  return (
+    <div>
+      <h2>Publicar un nuevo juego</h2>
+      <GameForm />
+    </div>
+  )
+}
